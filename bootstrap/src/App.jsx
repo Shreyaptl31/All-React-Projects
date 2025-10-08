4bb974e8685266effaa2a54456135c0003a58538
Shreyaptl31 <patelshreyu31@gmail.com>
2025-10-08 10:47:24 +0530

    first commit

---
+++ bootstrap/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
+function App() {
+
+  return (
+    <>
+      <h1>Shreya patel Implementing a Bootstrap template</h1>
+    </>
+  )
+}
+
+export default App
